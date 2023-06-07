8be99bbc2352953fa688ef2acd2132eeb0ef72aa
adrien.rey@outlook.com <adrien>
2023-06-07 09:06:29 +0200

    Simple REST service implemented with a user

--- backend/backend/src/main/java/tb/adrirey/backend/Controller.java
+++ backend/backend/src/main/java/tb/adrirey/backend/Controller.java
@@ -1,13 +1,34 @@
 package tb.adrirey.backend;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 @RestController
 public class Controller {
-    @GetMapping("/")
-    public String index() {
-        return "Hello world!";
+    User user = new User();
+
+    @RequestMapping("/userName")
+    public String getName() {
+        return user.getName() + " " + user.getSurname();
     }
 
+    @RequestMapping("/credit")
+    public float getCredit() {
+        return user.getCredit();
+    }
+
+    @RequestMapping("/userID")
+    public int getUserID() {
+        return user.getUserID();
+    }
+
+    @RequestMapping("/encryptData")
+    public String aesEncryptedData(@RequestParam String data) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        return user.aesEncryptedData(data);
+    }
 }
